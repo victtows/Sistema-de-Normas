c6f9a013e42c11ebec81222b0a01875486d7b09a
Fábio Augusto <fabioacv2403@gmail.com>
2026-05-21 09:38:01 -0300

    feat: criação da página de histórico e funcionamento do script de botões

---
+++ Frontend/script.js
@@ -0,0 +1,13 @@
+function abrirPagina(pagina){
+    if(pagina === 'iso27001'){
+        window.location.href = 'pagina27001.html';
+    } else if(pagina === 'iso27701'){
+        window.location.href = 'pagina27701.html';
+    } else if(pagina === 'historico'){
+        window.location.href = "historico.html";
+    } else {
+        alert("Página não encontrada!");
+    }
+}
+
+
