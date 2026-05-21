61c125328a69a0188411557d80626bbfd6520fa5
otaviopax <otaviopax@gmail.com>
2026-05-21 09:26:22 -0300

    feat: Adição de mensagem de conexão do banco de dados.

--- Backend/conexao.php
+++ Backend/conexao.php
@@ -1,8 +1,10 @@
 <?php
-$conexao = mysqli_connect("localhost", "root", "", "log");
+$conexao = mysqli_connect("localhost", "root", "", "log_de_seguranca");
 
 if (!$conexao) {
     die("Conexão Falhou: " . mysqli_connect_error());
+} else {
+    echo "<p> Banco de dados conectado. </p>";
 }
 
 mysqli_set_charset($conexao, "utf8");
