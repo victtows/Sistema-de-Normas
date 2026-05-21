7b63b2fa8b4fbfd4f9a9fa614e0e887d496ca35b
Fábio Augusto <fabioacv2403@gmail.com>
2026-05-21 10:43:17 -0300

    feat: criação inicial do backend

--- Backend/formulario.php
+++ Backend/formulario.php
@@ -1,3 +1,15 @@
 <?php
+    include("conexao.php");
 
+
+    if(isset($_POST["tipo_acao"]) && !empty($_POST["tipo_acao"])){
+        $tipo_acao = $_POST["tipo_acao"];
+
+        if($tipo_acao === "cadastro"){
+            ... 
+        } elseif($tipo_acao === "leitura"){
+            ...
+        }
+        
+    }
 ?>
