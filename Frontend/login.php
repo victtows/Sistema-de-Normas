5c1f8961e1a7db7a02d2ff1fb6fb8d8da0a70aeb
victtows <josevictorjustinosilva@gmail.com>
2026-05-25 15:59:10 -0300

    Feat: login

--- Frontend/login.php
+++ Frontend/login.php
@@ -25,7 +25,6 @@
     <div id="register" class="form">
         <h2>Criar conta</h2>
         <input type="text" placeholder="Nome">
-        <input type="text" placeholder="Usuário">
         <input type="email" placeholder="Email">
         <input type="password" placeholder="Mínimo 8 caracteres">
         <input type="password" placeholder="Confirme sua senha">
@@ -34,20 +33,6 @@
 
 </div>
 
-<script>
-function showTab(tab) {
-    document.querySelectorAll(".form").forEach(f => f.classList.remove("active"));
-    document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
-
-    document.getElementById(tab).classList.add("active");
-
-    if(tab === "login"){
-        document.querySelectorAll(".tab")[0].classList.add("active");
-    } else {
-        document.querySelectorAll(".tab")[1].classList.add("active");
-    }
-}
-</script>
-
+<script src="./login.js"></script>
 </body>
 </html>
